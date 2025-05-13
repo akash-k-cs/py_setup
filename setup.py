eebb8b6648079020a41478f04a42d2cefbe2c35f
akash kanojia <akash.kanojia@contentstack.com>
2025-05-13 17:25:28 +0530

    version update

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ class PostInstallCommand(install):
 
 setup(
     name='py_setup_tool',
-    version='0.1',
+    version='0.0.2',
     packages=['py_setup_tool'],
     install_requires=[],
     cmdclass={
